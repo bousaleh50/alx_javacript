6cf10fedabb497fd2c71b3226c4fbbd3c97f7f13
ahmed <bousalehahmed5@gmail.com>
2023-09-17 20:42:28 +0200

    update the file to accept the character id

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,12 +1,14 @@
 #!usr/bin/node
 const request = require("request");
 
+const character_id=process.argv[2];
+
 request.get("https://swapi-api.alx-tools.com/api/films/",(error,res,body)=>{
     const data = JSON.parse(res.body);
     const results=data.results.map(r=>r.characters)
     let nb_films=0;
     results.forEach(e => {
-        if(e.includes("https://swapi-api.alx-tools.com/api/people/18/")){
+        if(e.includes(`https://swapi-api.alx-tools.com/api/people/${character_id}/`)){
             nb_films++;
         }
     });
